d3d8d16c19290a793d1ae8e69c21aa3d4ec43def
earlvstheworld <B3.Dubb@gmail.com>
2026-09-11 13:56:04 -0400

    Add files via upload

---
+++ P1LAB1_WilliamsBobby.py
@@ -0,0 +1,15 @@
+# Bobby Williams
+# 09/10/2026    
+# Inputs and Outputs
+
+# Get first name 
+first_name = input("Enter your first name: ")
+
+# Get last name
+last_name = input("Enter your last name: ")
+
+# Output welcome message to user
+# Use concatenation in the output (+)
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110!")
+
+
